db991f71d124a46612950d3872eec51b8f88fd8a
yayanyang <yayan.yang@gh-ca.com>
2012-06-06 15:32:01 +0800

    modify ringbuffer interface
    Signed-off-by: yayanyang <yayan.yang@gh-ca.com>

--- memory/ringbuffer.hpp
+++ memory/ringbuffer.hpp
@@ -14,7 +14,7 @@
 
 namespace lemon{namespace memory{namespace ringbuffer{
 
-	template<size_t BlockSize,size_t Blocks = 1024>
+	template<size_t BlockSize,size_t Blocks = 1024,size_t BlocksPerPage = Blocks>
 	class allocator : private nocopyable
 	{
 	public:
@@ -22,7 +22,7 @@ namespace lemon{namespace memory{namespace ringbuffer{
 		{
 			LEMON_DECLARE_ERRORINFO(errorCode);
 
-			_allocator = LemonCreateRingBuffer(Blocks,BlockSize,&errorCode);
+			_allocator = LemonCreateRingBuffer(Blocks,BlockSize,BlocksPerPage,&errorCode);
 
 			if(LEMON_FAILED(errorCode)) throw Exception(errorCode);
 		}
@@ -30,6 +30,33 @@ namespace lemon{namespace memory{namespace ringbuffer{
 		~allocator()
 		{
 			LemonReleaseRingBuffer(_allocator);
+		}
+
+		size_t capacity() const 
+		{
+			return LemonRingBufferCapacity(_allocator);
+		}
+
+		size_t length() const
+		{
+			return LemonRingBufferLength(_allocator);
+		}
+
+		bool empty() const {return 0 == capacity();}
+
+		void * push_front(void * data,size_t dataLength)
+		{
+			return LemonRingBufferWriteFront(_allocator,data,dataLength);
+		}
+
+		void * push_back(void * data,size_t dataLength)
+		{
+			return LemonRingBufferWriteBack(_allocator,data,dataLength);
+		}
+
+		void direct_write(void * block,void * data,size_t dataLength)
+		{
+			LemonRingBufferDirectWrite(block,data,dataLength);
 		}
 
 	private:
